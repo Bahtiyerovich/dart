5179501df031d9afaed331c6d591808d3930381b
Kamola1188 <bahtiyarovich88@gmail.com>
2022-01-11 10:27:59 +0500

    initial

---
+++ main copy.dart
@@ -0,0 +1,70 @@
+//1
+// class Mine extends Object {
+//   String? n;
+//   Mine({this.n});
+
+//   DateTime _date = DateTime.now();
+//   String _name = 'New name';
+
+//   String get getDate => _date.toString().split('.')[0];
+
+//   String get getName => _name;
+//   void set setName(value) {
+//     _name = value;
+//   }
+//   void _takeNames(){}
+// }
+
+//MODEL
+// class Human extends Object {
+//   String? name;
+//   int? age;
+//   int? wight;
+//   double? height;
+
+//   Human({
+//     required this.name, // required named paratmetrs
+//     required this.age,
+//     required this.wight,
+//     required this.height,
+//   });
+// }
+
+//INHERITENCE
+
+// class Animal extends Object {
+//   String? color;
+//   void eat() {}
+// }
+
+// class Cat extends Animal {
+//   String? type;
+//   Cat({required this.type});
+
+//   void miaw() {}
+// }
+
+// class Dog extends Animal {
+//   String? breed;
+// }
+
+// super
+
+// class Car extends Object {
+//   String model;
+//   int speed;
+//   Car(this.model, this.speed);
+//   void otOldir() {}
+// }
+
+// class Tesla extends Car {
+//   double? price;
+//   String? m;
+//   int? s;
+//   Tesla({this.price, this.m, this.s}) : super(m!, s!);
+// }
+
+
+
+
+
